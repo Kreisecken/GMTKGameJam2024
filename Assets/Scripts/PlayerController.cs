7713908d681369c0fb572f35d913f4a05c901100
El_Tigere <eltigere@web.de>
2024-08-17 00:16:23 +0200

    implemented player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,20 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float movementSpeed = 3f;
+    
+    public InputAction moveAction;
+    
     void Start()
     {
+        moveAction = InputSystem.actions.FindAction("Move");
     }
 
     void Update()
     {
+        Vector2 movement = moveAction.ReadValue<Vector2>().normalized;
+        GetComponent<Rigidbody2D>().linearVelocity = movement * movementSpeed;
     }
 }
