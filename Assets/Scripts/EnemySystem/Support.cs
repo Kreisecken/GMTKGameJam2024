4020146a29aac08f97307ecf504ff9e227693a7b
El_Tigere <eltigere@web.de>
2024-08-17 18:14:53 +0200

    added support enemy

---
+++ Assets/Scripts/EnemySystem/Support.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Support : MonoBehaviour
+{
+    public float healHp = 5f;
+    public float healRange = 3f;
+    public float healDelay = 3f;
+    
+    private float healTimer = 0f;
+    
+    void Start()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        healTimer -= Time.fixedDeltaTime;
+        if(healTimer <= 0f) {
+            healTimer += healDelay;
+            List<Enemy> enemyList = Enemy.GetEnemiesInRangeSorted(transform.position, healRange);
+            foreach(Enemy enemy in enemyList) {
+                EnemyController ec = enemy.GetComponent<EnemyController>();
+                if(!ec.IsFullHp()) {
+                    ec.Heal(healHp);
+                    break;
+                }
+            }
+        }
+    }
+}
